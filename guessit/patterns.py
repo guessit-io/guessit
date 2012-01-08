b59f8a9f1a42370bab80a64db83a122789058983
Nicolas Wack <wackou@gmail.com>
2012-01-09 00:42:34 +0100

    fixed season pattern

--- guessit/patterns.py
+++ guessit/patterns.py
@@ -44,7 +44,8 @@ episode_rexps = [ # ... Season 2 ...
                   (r'[^0-9](?P<season>[0-9]{1,2})[x\.](?P<episodeNumber>[0-9]{2})[^0-9]', 0.8, (1, -1)),
 
                   # ... s02 ...
-                  (sep + r's(?P<season>[0-9]{1,2})' + sep + '?', 0.6, (1, -1)),
+                  (sep + r's(?P<season>[0-9]{1,2})' + sep, 0.6, (1, -1)),
+                  (sep + r's(?P<season>[0-9]{1,2})', 0.6, (1, 0)),
 
                   # v2 or v3 for some mangas which have multiples rips
                   (sep + r'(?P<episodeNumber>[0-9]{1,3})v[23]' + sep, 0.6, (0, 0)),
