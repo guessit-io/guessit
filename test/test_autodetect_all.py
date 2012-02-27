d33f5c9468d790d863c09a6c02cd92053fbaf8f4
Nicolas Wack <wackou@gmail.com>
2012-02-27 23:58:34 +0100

    added autodetection unittests (a lot still fail)

---
+++ test/test_autodetect_all.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# GuessIt - A library for guessing information from filenames
+# Copyright (c) 2012 Nicolas Wack <wackou@gmail.com>
+#
+# GuessIt is free software; you can redistribute it and/or modify it under
+# the terms of the Lesser GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# GuessIt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# Lesser GNU General Public License for more details.
+#
+# You should have received a copy of the Lesser GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+from guessittest import *
+
+def new_guesser(filename):
+    m = IterativeMatcher(filename, filetype = 'autodetect')
+    return m.matched()
+
+class TestAutoDetectAll(TestGuessit):
+
+    def testNewMatcher(self):
+        self.checkMinimumFieldsCorrect(new_guesser, 'autodetect.yaml', removeType = False)
+
+    def testAutoMatcherMovies(self):
+        self.checkMinimumFieldsCorrect(new_guesser, 'movies.yaml')
+
+    def testAutoMatcherEpisodes(self):
+        self.checkMinimumFieldsCorrect(new_guesser, 'episodes.yaml')
+
+
+suite = allTests(TestAutoDetectAll)
+
+if __name__ == '__main__':
+    TextTestRunner(verbosity=2).run(suite)
