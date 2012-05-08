01291bfa12e086dcddbe6d45b6fbabe7e39faf5e
Nicolas Wack <wackou@gmail.com>
2012-05-08 23:54:10 +0200

    Refactored a bit fabfile to accomodate for more generic test commands

--- fabfile.py
+++ fabfile.py
@@ -3,55 +3,68 @@
 import sys
 import re
 from fabric.api import *
+from fabric.tasks import Task
 
+@task
 def doctests():
-    """Run the doctests found in GuessIt classes."""
+    """Run the doctests found in GuessIt classes"""
     local('nosetests --with-doctest -vv guessit')
 
-def test_movie():
-    """Run the unittests for movies."""
-    local('PYTHONPATH=. python test/test_movie.py')
 
-def test_episode():
-    """Run the unittests for episodes."""
-    local('PYTHONPATH=. python test/test_episode.py')
+class TestTask(Task):
+    name = 'testtask'
+    def __init__(self, testname, docstring):
+        super(Task, self).__init__()
+        self.name = 'test_' + testname
+        self.__doc__ = 'Run the unittests for %s' % docstring
 
-def test_autodetect():
-    """Run the unittests for autodetected files."""
-    local('PYTHONPATH=. python test/test_autodetect.py')
+    def run(self):
+        local('PYTHONPATH=. python test/%s.py' % self.name)
 
-def test_language():
-    """Run the unittests for languages."""
-    local('PYTHONPATH=. python test/test_language.py')
+test_ep = TestTask('episode', 'episodes')
+test_movie = TestTask('movie', 'movies')
+test_auto = TestTask('autodetect', 'autodetected files')
+test_lang = TestTask('language', 'languages')
 
+
+@task
 def unittests():
-    """Run all the unittests."""
-    test_movie()
-    test_episode()
-    test_autodetect()
-    test_language()
+    """Run all the unittests"""
+    EXCLUDE = ['test_pypi_sdist']
+    def is_unittest(t):
+        return t[0].startswith('test_') and t[0] not in EXCLUDE
 
+    alltests = filter(is_unittest, globals().items())
+    for name, testcase in alltests:
+        testcase.run()
+
+
+@task
 def tests():
-    """Run both the doctests and the unittests."""
+    """Run both the doctests and the unittests"""
     doctests()
     unittests()
 
 
+@task
 def clean_pyc():
-    """Removes all the *.pyc files found in the repository."""
+    """Removes all the *.pyc files found in the repository"""
     local('find . -iname "*.pyc" -delete')
 
 
+@task
 def pylint():
-    """Runs pylint on GuessIt's source code. Only show problems, no report."""
+    """Runs pylint on GuessIt's source code. Only show problems, no report"""
     local('pylint --reports=n --include-ids=y --disable=C,I,W0703 guessit')
 
+
+@task
 def pylint_report():
-    """Runs pylint on GuessIt's source code, full report."""
+    """Runs pylint on GuessIt's source code, full report"""
     local('pylint --include-ids=y --disable=C0103,C0111 guessit')
 
 def open_file(filename):
-    """Open the given file using the OS's native programs."""
+    """Open the given file using the OS's native programs"""
     if sys.platform.startswith('linux'):
         local('xdg-open "%s"' % filename)
     elif sys.platform == 'darwin':
@@ -59,33 +72,41 @@ def open_file(filename):
     else:
         print 'Platform not supported:', sys.platform
 
+@task
 def doc():
-    """Build the Sphinx documentation and open it in a web browser."""
+    """Build the Sphinx documentation and open it in a web browser"""
     with lcd('docs'):
         local('make html')
         open_file('_build/html/index.html')
 
+@task
 def pypi_doc():
     """Builds the main page that will be uploaded to PyPI and open it in a
-    web browser."""
+    web browser"""
     local('python setup.py --long-description | rst2html.py > /tmp/guessit_pypi_doc.html')
     open_file('/tmp/guessit_pypi_doc.html')
 
 
 # Release management functions
 
+@task
 def set_version(version):
-    """Set the version in the guessit/__init__.py file."""
+    """Set the version in the guessit/__init__.py file"""
     initfile = open('guessit/__init__.py').read()
     initfile = re.sub(r"__version__ = '\S*'",
                       r"__version__ = '%s'" % version,
                       initfile)
     open('guessit/__init__.py', 'w').write(initfile)
 
+@task
 def upload_pypi():
+    """Build and upload the package on PyPI"""
     local('python setup.py register sdist upload')
 
+@task
 def test_pypi_sdist():
+    """Build the PyPI package and test whether it is installable and passes
+    the tests"""
     d = '_tmp_pypi_guessit'
     local('rm -fr dist %s' % d)
     local('python setup.py sdist')
