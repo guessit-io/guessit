6e27584f406e0e11f1ba9fa305cd23ea538ef1d7
Nicolas Wack <wackou@gmail.com>
2012-07-17 23:30:42 +0200

    Added script to run tests on python3 (fabric has not been ported yet)

---
+++ .travis.runtests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import subprocess
+import os
+
+def local(cmd, envvars=None):
+    if not envvars:
+        subprocess.Popen(cmd.split()).communicate()
+        return
+
+    env = os.environ
+    for var, value in envvars.items():
+        env[var] = value
+    subprocess.Popen(cmd.split(), env=env).communicate()
+
+
+
+def doctests():
+    local('nosetests --with-doctest -vv guessit')
+
+
+TESTS = ['episode', 'movie', 'autodetect', 'language', 'utils']
+
+def unittests():
+    for t in TESTS:
+        local('python tests/test_%s.py' % t, envvars={'PYTHONPATH': '.'})
+
+if __name__ == '__main__':
+    doctests()
+    unittests()
