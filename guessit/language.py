48a2813399804b7bc1b9ed1bd75acc005207612c
Nicolas Wack <wackou@gmail.com>
2013-05-06 22:41:21 +0200

    Added more language patterns

--- guessit/language.py
+++ guessit/language.py
@@ -326,7 +326,7 @@ def search_language(string, lang_filter=None):
         'la', 'el', 'del', 'por', 'mar',
         # other
         'ind', 'arw', 'ts', 'ii', 'bin', 'chan', 'ss', 'san', 'oss', 'iii',
-        'vi', 'ben'
+        'vi', 'ben', 'da'
         ])
     sep = r'[](){} \._-+'
 
