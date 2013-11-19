0be810ad213bee51343b946a9d5a98bbc467f455
Toilal <toilal.dev@gmail.com>
2013-11-19 08:29:55 +0100

    properties API completed

--- guessit/patterns/properties.py
+++ guessit/patterns/properties.py
@@ -25,174 +25,124 @@ from . import sep
 from guessit import base_text_type
 import re
 
-prop_multi = { 'format': { 'DVD': [ 'DVD', 'DVD-Rip', 'VIDEO-TS', 'DVDivX' ],
-                           'HD-DVD': [ 'HD-(?:DVD)?-Rip', 'HD-DVD' ],
-                           'BluRay': [ 'Blu-ray', 'B[DR]Rip' ],
-                           'HDTV': [ 'HD-TV' ],
-                           'DVB': [ 'DVB-Rip', 'DVB', 'PD-TV' ],
-                           'WEBRip': [ 'WEB-Rip' ],
-                           'Screener': [ 'DVD-SCR', 'Screener' ],
-                           'VHS': [ 'VHS' ],
-                           'WEB-DL': [ 'WEB-DL' ] },
+_properties = {}
+_properties_compiled = {}
 
-               'is3D': { True: [ '3D' ] },
+_dash = '-'
+_psep = '[-. _]?'
 
-               'screenSize': { '360p': [ '(?:\d{3,}(?:\\|\/|x|\*))?360(?:i|p?x?)' ],
-                               '368p': [ '(?:\d{3,}(?:\\|\/|x|\*))?368(?:i|p?x?)' ],
-                               '480p': [ '(?:\d{3,}(?:\\|\/|x|\*))?480(?:i|p?x?)' ],
-                               '576p': [ '(?:\d{3,}(?:\\|\/|x|\*))?576(?:i|p?x?)' ],
-                               '720p': [ '(?:\d{3,}(?:\\|\/|x|\*))?720(?:i|p?x?)' ],
-                               '1080i': [ '(?:\d{3,}(?:\\|\/|x|\*))?1080i' ],
-                               '1080p': [ '(?:\d{3,}(?:\\|\/|x|\*))?1080(?:\z|[^i])p?x?' ],
-                               '4K': [ '(?:\d{3,}(?:\\|\/|x|\*))?2160(?:\z|[^i])p?x?'] },
-
-               'videoCodec': { 'XviD': [ 'Xvid' ],
-                               'DivX': [ 'DVDivX', 'DivX' ],
-                               'h264': [ '[hx]-264' ],
-                               'Rv10': [ 'Rv10' ],
-                               'Mpeg2': [ 'Mpeg2' ] },
-
-               # has nothing to do here (or on filenames for that matter), but some
-               # releases use it and it helps to identify release groups, so we adapt
-               'videoApi': {  'DXVA': [ 'DXVA' ] },
-
-               'audioCodec': { 'AC3': [ 'AC3' ],
-                               'DTS': [ 'DTS' ],
-                               'AAC': [ 'He-AAC', 'AAC-He', 'AAC' ] },
-
-               'audioChannels': { '5.1': [ r'5\.1', 'DD5[._ ]1', '5ch' ] },
-
-               'episodeFormat': { 'Minisode': [ 'Minisodes?' ] }
-
-               }
-
-# prop_single dict of { property_name: [ canonical_form ] }
-prop_single = { 'releaseGroup': [ 'ESiR', 'WAF', 'SEPTiC', r'\[XCT\]', 'iNT', 'PUKKA',
-                                  'CHD', 'ViTE', 'TLF', 'FLAiTE',
-                                  'MDX', 'GM4F', 'DVL', 'SVD', 'iLUMiNADOS',
-                                  'aXXo', 'KLAXXON', 'NoTV', 'ZeaL', 'LOL',
-                                  'CtrlHD', 'POD', 'WiKi','IMMERSE', 'FQM',
-                                  '2HD',  'CTU', 'HALCYON', 'EbP', 'SiTV',
-                                  'HDBRiSe', 'AlFleNi-TeaM', 'EVOLVE', '0TV',
-                                  'TLA', 'NTB', 'ASAP', 'MOMENTUM', 'FoV', 'D-Z0N3',
-                                  'TrollHD', 'ECI'
-                                  ],
-
-                # potentially confusing release group names (they are words)
-                'weakReleaseGroup': [ 'DEiTY', 'FiNaLe', 'UnSeeN', 'KiNGS', 'CLUE', 'DIMENSION',
-                                      'SAiNTS', 'ARROW', 'EuReKA', 'SiNNERS', 'DiRTY', 'REWARD',
-                                      'REPTiLE',
-                                      ],
-
-                'other': [ 'PROPER', 'REPACK', 'LIMITED', 'DualAudio', 'Audiofixed', 'R5',
-                           'complete', 'classic', # not so sure about these ones, could appear in a title
-                           'ws' ] # widescreen
-                }
-
-__dash = '-'
-__psep = '[-. _]?'
 
 def compile_pattern(pattern):
     return re.compile(enhance_pattern(pattern), re.IGNORECASE)
 
+
 def enhance_pattern(pattern):
     """
     Enhance pattern to match more equivalent values.
-    
+
     @param pattern: string considered as a regexp.
-    
+
     '-' are replaced by ([ \.-_]), which matches more types of separators (or none)
     """
-    return pattern.replace(__dash, __psep)
+    return pattern.replace(_dash, _psep)
+
 
 def enhance_property_patterns(name):
     """
-    Get the enhanced pattern of given property.
-    
-    @param name: property name of patterns to enhance. 
-    
+    Get the enhanced patterns of given property.
+
+    @param name: property name of patterns to enhance.
+
     @see enhance_pattern(pattern)
     """
-    return [ enhance_pattern(p) for patterns in prop_multi[name].values() for p in patterns  ]
+    return [enhance_pattern(p) for patterns in _properties[name].values() for p in patterns]
 
-def unregister_property_patterns(name, canonical_form):
+
+def unregister_property(name, canonical_form):
     """
     Unregister a property pattern canonical_form
     """
-    prop_canonical_forms = prop_multi.get(name)
-    
-    if not prop_canonical_forms is None:
-        prop_patterns = prop_canonical_forms.get(canonical_form)
-        
-        if not prop_patterns is None:
-            del prop_canonical_forms[canonical_form]
-        
-        if not prop_canonical_forms:
-            del prop_multi[name]
+    prop_canonical_forms = _properties.get(name)
 
-def register_property_pattern(name, canonical_form, *patterns):
+    if not prop_canonical_forms is None:
+        if canonical_form in prop_canonical_forms:
+            del prop_canonical_forms[canonical_form]
+
+        if not prop_canonical_forms:
+            del _properties[name]
+
+    rexps = _properties_compiled.get(name)
+    if not rexps is None:
+        if canonical_form in rexps:
+            del rexps[canonical_form]
+
+        if not rexps:
+            del _properties_compiled[name]
+
+
+def register_property(name, canonical_form, *patterns):
     """
-    Register a property pattern canonical_form
-    
+    Register property with defined canonical form and multiple patterns.
+
     @param name: name of the property (format, screenSize, ...)
     @param canonical_form: value of the property (DVD, 720p, ...)
     @param patterns: regular expression patterns to register for the property canonical_form
-    
+
     @note: simpler patterns need to be at the end of the list to not shadow more
     complete ones, eg: 'AAC' needs to come after 'He-AAC'
     ie: from most specific to less specific
     """
-    global prop_multi
-    
-    prop_canonical_forms = prop_multi.get(name)
-    
+    global _properties
+    prop_canonical_forms = _properties.get(name)
+
     if prop_canonical_forms is None:
         prop_canonical_forms = {}
-        prop_multi[name] = prop_canonical_forms
+        _properties[name] = prop_canonical_forms
     prop_patterns = prop_canonical_forms.get(canonical_form)
-    
+
     if prop_patterns is None:
         prop_patterns = []
         prop_canonical_forms[canonical_form] = prop_patterns
-    
+
     for pattern in patterns:
         prop_patterns.append(pattern)
-        
-__properties_rexps = None
 
-def compile_all():
+    rexps = _properties_compiled.get(name)
+    if rexps is None:
+        rexps = {}
+        _properties_compiled[name] = rexps
+
+    property_patterns = None
+    if not canonical_form in rexps:
+        property_patterns = []
+        rexps[canonical_form] = property_patterns
+    else:
+        property_patterns = rexps[canonical_form]
+    for pattern in patterns:
+        property_patterns.append(compile_pattern(pattern))
+
+
+def register_properties(name, *canonical_forms):
     """
-    After changing patterns in this file, this method should be called before trying to guess anything.
-    
-    It compiles defined properties (prop_multi & prop_single) into patterns.__properties_rexps
-    
-    @return __properties_rexps dict of { property_name: { canonical_form: [ rexp ] } }
-    containing the rexps compiled from both prop_multi and prop_single
+    Registry properties.
+
+    @param name: name of the property (releaseGroup, ...)
+    @param canonical_forms: values of the property ('ESiR', 'WAF', 'SEPTiC', ...)
     """
-    global __properties_rexps
-    __properties_rexps = dict((type_, dict((canonical_form,
-                                         [ compile_pattern(pattern) for pattern in patterns ])
-                                        for canonical_form, patterns in props.items()))
-                            for type_, props in prop_multi.items())
+    for canonical_form in canonical_forms:
+        register_property(name, canonical_form, canonical_form)
 
-    __properties_rexps.update(dict((type_, dict((canonical_form, [ compile_pattern(canonical_form) ])
-                                             for canonical_form in props))
-                                 for type_, props in prop_single.items()))
-
-    return __properties_rexps
-
-compile_all()
 
 def find_properties(string):
     """
     Find properties for given string.
-    
+
     A property must always be surrounded by separators to be returned.
-    
+
     @return: list of tuple (property_name, canonical_form, start, end)
     """
     result = []
-    for property_name, props in __properties_rexps.items():
+    for property_name, props in _properties_compiled.items():
         # FIXME: this should be done in a more flexible way...
         if property_name in ['weakReleaseGroup']:
             continue
@@ -205,20 +155,80 @@ def find_properties(string):
                     # make sure our word is always surrounded by separators
                     # note: sep is a regexp, but in this case using it as
                     #       a char sequence achieves the same goal
-                    if ((start > 0 and string[start-1] not in sep) or
+                    if ((start > 0 and string[start - 1] not in sep) or
                         (end < len(string) and string[end] not in sep)):
                         continue
 
                     result.append((property_name, canonical_form, start, end))
     return result
 
+
 def compute_canonical_form(property_name, value):
     """
     @return: Canonical form of a property given its name if it is a valid one, None otherwise.
     """
     if isinstance(value, base_text_type):
-        for canonical_form, rexps in __properties_rexps[property_name].items():
+        for canonical_form, rexps in _properties_compiled[property_name].items():
             for rexp in rexps:
                 if rexp.match(value):
                     return canonical_form
     return None
+
+register_property('format', 'DVD', 'DVD', 'DVD-Rip', 'VIDEO-TS', 'DVDivX')
+register_property('format', 'HD-DVD', 'HD-(?:DVD)?-Rip', 'HD-DVD')
+register_property('format', 'BluRay', 'Blu-ray', 'B[DR]Rip')
+register_property('format', 'HDTV', 'HD-TV')
+register_property('format', 'DVB', 'DVB-Rip', 'DVB', 'PD-TV')
+register_property('format', 'WEBRip', 'WEB-Rip')
+register_property('format', 'Screener', 'DVD-SCR', 'Screener')
+register_property('format', 'VHS', 'VHS')
+register_property('format', 'WEB-DL', 'WEB-DL')
+
+register_property('is3D', True, '3D')
+
+register_property('screenSize', '360p', '(?:\d{3,}(?:\\|\/|x|\*))?360(?:i|p?x?)')
+register_property('screenSize', '368p', '(?:\d{3,}(?:\\|\/|x|\*))?368(?:i|p?x?)')
+register_property('screenSize', '480p', '(?:\d{3,}(?:\\|\/|x|\*))?480(?:i|p?x?)')
+register_property('screenSize', '576p', '(?:\d{3,}(?:\\|\/|x|\*))?576(?:i|p?x?)')
+register_property('screenSize', '720p', '(?:\d{3,}(?:\\|\/|x|\*))?720(?:i|p?x?)')
+register_property('screenSize', '1080i', '(?:\d{3,}(?:\\|\/|x|\*))?1080i(?:i|p?x?)')
+register_property('screenSize', '1080p', '(?:\d{3,}(?:\\|\/|x|\*))?1080(?:i|p?x?)')
+register_property('screenSize', '4K', '(?:\d{3,}(?:\\|\/|x|\*))?2160(?:i|p?x?)')
+
+register_property('videoCodec', 'XviD', 'Xvid')
+register_property('videoCodec', 'DivX', 'DVDivX', 'DivX')
+register_property('videoCodec', 'h264', '[hx]-264')
+register_property('videoCodec', 'Rv10', 'Rv10')
+register_property('videoCodec', 'Mpeg2', 'Mpeg2')
+
+# has nothing to do here (or on filenames for that matter), but some
+# releases use it and it helps to identify release groups, so we adapt
+register_property('videoApi', 'DXVA', 'DXVA')
+
+register_property('audioCodec', 'AC3', 'AC3')
+register_property('audioCodec', 'DTS', 'DTS')
+register_property('audioCodec', 'AAC', 'He-AAC', 'AAC-He', 'AAC')
+
+register_property('audioChannels', '5.1', r'5\.1', 'DD5[._ ]1', '5ch')
+
+register_property('episodeFormat', 'Minisode', r'Minisodes?')
+
+register_properties('releaseGroup', 'ESiR', 'WAF', 'SEPTiC', r'\[XCT\]', 'iNT', 'PUKKA',
+                                  'CHD', 'ViTE', 'TLF', 'FLAiTE',
+                                  'MDX', 'GM4F', 'DVL', 'SVD', 'iLUMiNADOS',
+                                  'aXXo', 'KLAXXON', 'NoTV', 'ZeaL', 'LOL',
+                                  'CtrlHD', 'POD', 'WiKi', 'IMMERSE', 'FQM',
+                                  '2HD', 'CTU', 'HALCYON', 'EbP', 'SiTV',
+                                  'HDBRiSe', 'AlFleNi-TeaM', 'EVOLVE', '0TV',
+                                  'TLA', 'NTB', 'ASAP', 'MOMENTUM', 'FoV', 'D-Z0N3',
+                                  'TrollHD', 'ECI'
+                                  )
+
+register_properties('weakReleaseGroup', 'DEiTY', 'FiNaLe', 'UnSeeN', 'KiNGS', 'CLUE', 'DIMENSION',
+                                      'SAiNTS', 'ARROW', 'EuReKA', 'SiNNERS', 'DiRTY', 'REWARD',
+                                      'REPTiLE',
+                                      )
+
+register_properties('other', 'PROPER', 'REPACK', 'LIMITED', 'DualAudio', 'Audiofixed', 'R5',
+                           'complete', 'classic',  # not so sure about these ones, could appear in a title
+                           'ws')
