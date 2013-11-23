96a9eceb34969801c20150693eadc52a4ca0dce1
Toilal <toilal.dev@gmail.com>
2013-11-23 17:46:59 +0100

    Avoid invalid language detection that split titles. Fix #67

--- guessit/__init__.py
+++ guessit/__init__.py
@@ -128,11 +128,26 @@ def _guess_filename(filename, filetype):
 
     to_skip_language_nodes = []
 
-    title_nodes = set(n for n in find_nodes(mtree.match_tree, ['title', 'series']))
-    title_spans = {}
-    for title_node in title_nodes:
-        title_spans[title_node.span[0]] = title_node
-        title_spans[title_node.span[1]] = title_node
+    unidentified_starts = {}
+    unidentified_ends = {}
+
+    property_starts = {}
+    property_ends = {}
+
+    title_starts = {}
+    title_ends = {}
+
+    for unidentified_node in mtree.match_tree.unidentified_leaves():
+        unidentified_starts[unidentified_node.span[0]] = unidentified_node
+        unidentified_ends[unidentified_node.span[1]] = unidentified_node
+
+    for property_node in find_nodes(mtree.match_tree, ['year']):
+        property_starts[property_node.span[0]] = property_node
+        property_ends[property_node.span[1]] = property_node
+
+    for title_node in find_nodes(mtree.match_tree, ['title', 'series']):
+        title_starts[title_node.span[0]] = title_node
+        title_ends[title_node.span[1]] = title_node
 
     for lang_key in ('language', 'subtitleLanguage'):
         langs = {}
@@ -140,8 +155,9 @@ def _guess_filename(filename, filetype):
 
         for lang_node in lang_nodes:
             lang = lang_node.guess.get(lang_key, None)
-            if len(lang_node.value) > 3 and (lang_node.span[0] in title_spans.keys() or lang_node.span[1] in title_spans.keys()):
-                # Language is next or before title, and is not a language code. Add to skip for 2nd pass.
+            if lang_node.span[0] in title_ends.keys() and (lang_node.span[1] in unidentified_starts.keys() or lang_node.span[1] + 1 in property_starts.keys()) or\
+               lang_node.span[1] in title_starts.keys() and (lang_node.span[0] == 0 or lang_node.span[0] in unidentified_ends.keys() or lang_node.span[0] in property_ends.keys()):
+                # Language probably split the title. Add to skip for 2nd pass.
 
                 # if filetype is subtitle and the language appears last, just before
                 # the extension, then it is likely a subtitle language
