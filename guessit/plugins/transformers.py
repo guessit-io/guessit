6ed14d64cf2650965bcccc65501b5bcde68728db
Toilal <toilal.dev@gmail.com>
2014-01-21 08:08:42 +0100

    Add custom extension manager for transformers

--- guessit/plugins/transformers.py
+++ guessit/plugins/transformers.py
@@ -30,7 +30,7 @@ def _is_enabled(extension):
     return extension.obj.enabled()
 
 
-class TransformerExtensionManager(EnabledExtensionManager):
+class CustomTransformerExtensionManager(EnabledExtensionManager):
     def __init__(self, namespace='guessit.transformer', check_func=_is_enabled, invoke_on_load=True,
         invoke_args=(), invoke_kwds={},
         propagate_map_exceptions=True):
@@ -39,38 +39,13 @@ class TransformerExtensionManager(EnabledExtensionManager):
                                          propagate_map_exceptions=propagate_map_exceptions)
 
     def __iter__(self):
-        return EnabledExtensionManager.__iter__(self)
+        return super(EnabledExtensionManager, self).__iter__(self)
 
     def objects(self):
         return self.map(self._get_obj)
 
     def _find_entry_points(self, namespace):
         entry_points = {}
-        # Internal entry points
-        if namespace == 'guessit.transformer':
-            internal_entry_points_str = ['split_path_components = guessit.transfo.split_path_components:SplitPathComponents',
-                                    'guess_filetype = guessit.transfo.guess_filetype:GuessFiletype',
-                                    'split_explicit_groups = guessit.transfo.split_explicit_groups:SplitExplicitGroups',
-                                    'guess_date = guessit.transfo.guess_date:GuessDate',
-                                    'guess_website = guessit.transfo.guess_website:GuessWebsite',
-                                    'guess_release_group = guessit.transfo.guess_release_group:GuessReleaseGroup',
-                                    'guess_properties = guessit.transfo.guess_properties:GuessProperties',
-                                    'guess_language = guessit.transfo.guess_language:GuessLanguage',
-                                    'guess_video_rexps = guessit.transfo.guess_video_rexps:GuessVideoRexps',
-                                    'guess_episodes_rexps = guessit.transfo.guess_episodes_rexps:GuessEpisodesRexps',
-                                    'guess_weak_episodes_rexps = guessit.transfo.guess_weak_episodes_rexps:GuessWeakEpisodesRexps',
-                                    'guess_bonus_features = guessit.transfo.guess_bonus_features:GuessBonusFeatures',
-                                    'guess_year = guessit.transfo.guess_year:GuessYear',
-                                    'guess_country = guessit.transfo.guess_country:GuessCountry',
-                                    'guess_idnumber = guessit.transfo.guess_idnumber:GuessIdnumber',
-                                    'split_on_dash = guessit.transfo.split_on_dash:SplitOnDash',
-                                    'guess_episode_info_from_position = guessit.transfo.guess_episode_info_from_position:GuessEpisodeInfoFromPosition',
-                                    'guess_movie_title_from_position = guessit.transfo.guess_movie_title_from_position:GuessMovieTitleFromPosition',
-                                    'post_process = guessit.transfo.post_process:PostProcess']
-            for internal_entry_point_str in internal_entry_points_str:
-                internal_entry_point = EntryPoint.parse(internal_entry_point_str)
-                entry_points[internal_entry_point.name] = internal_entry_point
-
         # Package entry points
         setuptools_entrypoints = super(EnabledExtensionManager, self)._find_entry_points(namespace)
         for setuptools_entrypoint in setuptools_entrypoints:
@@ -109,4 +84,57 @@ class TransformerExtensionManager(EnabledExtensionManager):
     def _get_obj(self, ext):
         return ext.obj
 
-extensions = TransformerExtensionManager()
+
+class DefaultTransformerExtensionManager(CustomTransformerExtensionManager):
+    def _find_entry_points(self, namespace):
+        entry_points = {}
+        # Internal entry points
+        if namespace == 'guessit.transformer':
+            internal_entry_points_str = ['split_path_components = guessit.transfo.split_path_components:SplitPathComponents',
+                                    'guess_filetype = guessit.transfo.guess_filetype:GuessFiletype',
+                                    'split_explicit_groups = guessit.transfo.split_explicit_groups:SplitExplicitGroups',
+                                    'guess_date = guessit.transfo.guess_date:GuessDate',
+                                    'guess_website = guessit.transfo.guess_website:GuessWebsite',
+                                    'guess_release_group = guessit.transfo.guess_release_group:GuessReleaseGroup',
+                                    'guess_properties = guessit.transfo.guess_properties:GuessProperties',
+                                    'guess_language = guessit.transfo.guess_language:GuessLanguage',
+                                    'guess_video_rexps = guessit.transfo.guess_video_rexps:GuessVideoRexps',
+                                    'guess_episodes_rexps = guessit.transfo.guess_episodes_rexps:GuessEpisodesRexps',
+                                    'guess_weak_episodes_rexps = guessit.transfo.guess_weak_episodes_rexps:GuessWeakEpisodesRexps',
+                                    'guess_bonus_features = guessit.transfo.guess_bonus_features:GuessBonusFeatures',
+                                    'guess_year = guessit.transfo.guess_year:GuessYear',
+                                    'guess_country = guessit.transfo.guess_country:GuessCountry',
+                                    'guess_idnumber = guessit.transfo.guess_idnumber:GuessIdnumber',
+                                    'split_on_dash = guessit.transfo.split_on_dash:SplitOnDash',
+                                    'guess_episode_info_from_position = guessit.transfo.guess_episode_info_from_position:GuessEpisodeInfoFromPosition',
+                                    'guess_movie_title_from_position = guessit.transfo.guess_movie_title_from_position:GuessMovieTitleFromPosition',
+                                    'post_process = guessit.transfo.post_process:PostProcess']
+            for internal_entry_point_str in internal_entry_points_str:
+                internal_entry_point = EntryPoint.parse(internal_entry_point_str)
+                entry_points[internal_entry_point.name] = internal_entry_point
+
+        # Package entry points
+        setuptools_entrypoints = super(EnabledExtensionManager, self)._find_entry_points(namespace)
+        for setuptools_entrypoint in setuptools_entrypoints:
+            entry_points[setuptools_entrypoint.name] = setuptools_entrypoint
+
+        return list(entry_points.values())
+
+extensions = None
+
+
+def reload_manager(custom=False):
+    """
+    Reload extension manager with default or custom one.
+    :param custom: if True, custom manager will be used, else default one.
+    Default manager will load default extensions from guessit and setuptools packaging extensions
+    Custom manager will not load default extensions from guessit, using only setuptools packaging extensions.
+    :type custom: boolean
+    """
+    global extensions
+    if custom:
+        extensions = CustomTransformerExtensionManager()
+    else:
+        extensions = DefaultTransformerExtensionManager()
+
+reload_manager()
