ecbf708c68c1d5d50334bfac5a7112ce7dd1b4db
Toilal <toilal.dev@gmail.com>
2014-02-21 23:41:02 +0100

    Use PropertiesContainer in guess_weak_episodes_rexps

--- guessit/transfo/guess_weak_episodes_rexps.py
+++ guessit/transfo/guess_weak_episodes_rexps.py
@@ -21,55 +21,45 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 from guessit.plugins.transformers import Transformer, SingleNodeGuesser
-from guessit.guess import Guess
 from guessit.patterns import sep
+from guessit.patterns.containers import PropertiesContainer
 from guessit.patterns.numeral import numeral, parse_numeral
 from guessit.date import valid_year
-import re
 
 
 class GuessWeakEpisodesRexps(Transformer):
     def __init__(self):
         Transformer.__init__(self, 15)
 
-        self._weak_episode_rexps = [  # ... 213 or 0106 ...
-                       (sep + r'(?P<episodeNumber>[0-9]{2,4})' + sep, (1, -1), None),
-                        # ... episode 4 ...
-                        ('(?:episode)' + sep + r'(?P<episodeNumber>' + numeral + ')[^0-9]', (0, -1), self._has_season),
-                       ]
+        self.properties = PropertiesContainer(enhance=False, canonical_from_pattern=False)
 
-    def _has_season(self, string, node):
-        return node and 'season' in node.root.info
+        def _formater(episodeNumber):
+            epnum = parse_numeral(episodeNumber)
+            if not valid_year(epnum):
+                if epnum > 100:
+                    season, epnum = epnum // 100, epnum % 100
+                    # episodes which have a season > 50 are most likely errors
+                    # (Simpson is at 25!)
+                    if season > 50:
+                        return None
+                    return {'season': season, 'episodeNumber': epnum}
+                else:
+                    return epnum
+
+        self.properties.register_property(['episodeNumber', 'season'], '[0-9]{2,4}', confidence=0.6, formatter=_formater)
+        self.properties.register_property('episodeNumber', '(?:episode)' + sep + '(' + numeral + ')[^0-9]', confidence=0.3)
 
     def supported_properties(self):
-        return ['episodeNumber', 'season']
+        return self.properties.get_supported_properties()
 
     def guess_weak_episodes_rexps(self, string, node=None, options=None):
         if node and 'episodeNumber' in node.root.info:
             return None
 
-        for rexp, span_adjust, _validation_func in self._weak_episode_rexps:
-            if not _validation_func or _validation_func(string, node):
-                match = re.search(rexp, string, re.IGNORECASE)
-                if match:
-                    metadata = match.groupdict()
-                    span = (match.start() + span_adjust[0],
-                            match.end() + span_adjust[1])
+        properties = self.properties.find_properties(string, node)
+        guess = self.properties.as_guess(properties, string)
 
-                    epnum = parse_numeral(metadata['episodeNumber'])
-                    metadata['episodeNumber'] = epnum
-                    if not valid_year(epnum):
-                        if epnum > 100:
-                            season, epnum = epnum // 100, epnum % 100
-                            # episodes which have a season > 50 are most likely errors
-                            # (Simpson is at 25!)
-                            if season > 50:
-                                continue
-                            return Guess({'season': season, 'episodeNumber': epnum}, confidence=0.6, input=string, span=span)
-                        else:
-                            return Guess(metadata, confidence=0.3, input=string, span=span)
-
-        return None
+        return guess
 
     def should_process(self, mtree, options=None):
         return mtree.guess.get('type', '').startswith('episode')
