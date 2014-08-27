d017d937a569297eae6660202ded9a1dbd553063
Toilal <toilal.dev@gmail.com>
2014-08-27 08:54:34 +0200

    Create options groups

--- guessit/options.py
+++ guessit/options.py
@@ -1,33 +1,48 @@
-from optparse import OptionParser
+from optparse import OptionParser, OptionGroup
 
 option_parser = OptionParser(usage='usage: %prog [options] file1 [file2...]')
-option_parser.add_option('', '--date-year-first', action='store_true', dest='date_year_first', default=False,
-                         help='If short date is found, consider the first digits as the year.')
-option_parser.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False,
-                         help='Display debug output')
-option_parser.add_option('-p', '--properties', dest='properties', action='store_true', default=False,
-                         help='Display properties that can be guessed.')
-option_parser.add_option('-l', '--values', dest='values', action='store_true', default=False,
-                         help='Display property values that can be guessed.')
-option_parser.add_option('-s', '--transformers', dest='transformers', action='store_true', default=False,
-                         help='Display transformers that can be used.')
-option_parser.add_option('-c', '--split-camel', dest='split_camel', action='store_true', default=False,
+
+name_format_group = OptionGroup(option_parser, "Naming")
+option_parser.add_option_group(name_format_group)
+name_format_group.add_option('-t', '--type', dest='type', default=None,
+                         help='The suggested file type: movie, episode. If undefined, type will be guessed.')
+name_format_group.add_option('-n', '--name-only', dest='name_only', action='store_true', default=False,
+                         help='Parse files as name only. Disable folder parsing, extension parsing, and file content analysis.')
+name_format_group.add_option('-c', '--split-camel', dest='split_camel', action='store_true', default=False,
                          help='Split camel case part of filename.')
-option_parser.add_option('-i', '--info', dest='info', default='filename',
+name_format_group.add_option('', '--date-year-first', action='store_true', dest='date_year_first', default=False,
+                         help='If short date is found, consider the first digits as the year.')
+
+mode_group = OptionGroup(option_parser, "Mode")
+option_parser.add_option_group(mode_group)
+mode_group.add_option('-i', '--info', dest='info', default='filename',
                          help='The desired information type: filename, video, hash_mpc or a hash from python\'s '
                               'hashlib module, such as hash_md5, hash_sha1, ...; or a list of any of '
                               'them, comma-separated')
-option_parser.add_option('-n', '--name-only', dest='name_only', action='store_true', default=False,
-                         help='Parse files as name only. Disable folder parsing, extension parsing, and file content analysis.')
-option_parser.add_option('-t', '--type', dest='type', default=None,
-                         help='The suggested file type: movie, episode. If undefined, type will be guessed.')
-option_parser.add_option('-a', '--advanced', dest='advanced', action='store_true', default=False,
+
+output_group = OptionGroup(option_parser, "Output")
+option_parser.add_option_group(output_group)
+output_group.add_option('-v', '--verbose', action='store_true', dest='verbose', default=False,
+                         help='Display debug output')
+output_group.add_option('-a', '--advanced', dest='advanced', action='store_true', default=False,
                          help='Display advanced information for filename guesses, as json output')
-option_parser.add_option('-y', '--yaml', dest='yaml', action='store_true', default=False,
+output_group.add_option('-y', '--yaml', dest='yaml', action='store_true', default=False,
                          help='Display information for filename guesses as yaml output (like unit-test)')
-option_parser.add_option('-f', '--input-file', dest='input_file', default=False,
+output_group.add_option('-f', '--input-file', dest='input_file', default=False,
                          help='Read filenames from an input file.')
-option_parser.add_option('-d', '--demo', action='store_true', dest='demo', default=False,
+output_group.add_option('-d', '--demo', action='store_true', dest='demo', default=False,
                          help='Run a few builtin tests instead of analyzing a file')
-option_parser.add_option('-b', '--bug', action='store_true', dest='submit_bug', default=False,
+
+information_group = OptionGroup(option_parser, "Information")
+option_parser.add_option_group(information_group)
+information_group.add_option('-p', '--properties', dest='properties', action='store_true', default=False,
+                         help='Display properties that can be guessed.')
+information_group.add_option('-l', '--values', dest='values', action='store_true', default=False,
+                         help='Display property values that can be guessed.')
+information_group.add_option('-s', '--transformers', dest='transformers', action='store_true', default=False,
+                         help='Display transformers that can be used.')
+
+webservice_group = OptionGroup(option_parser, "guessit.io")
+option_parser.add_option_group(webservice_group)
+webservice_group.add_option('-b', '--bug', action='store_true', dest='submit_bug', default=False,
                          help='Submit a wrong detection to the guessit.io service')
