abeda01444414a3cb62f0cde9ebe46e75d08799e
Toilal <toilal.dev@gmail.com>
2015-10-15 20:48:54 +0200

    Use Rule class for title property

--- guessit/rules/title.py
+++ guessit/rules/title.py
@@ -3,26 +3,25 @@
 """
 Title
 """
-from rebulk import Rebulk
+from rebulk import Rebulk, Rule
 
 from .common.formatters import cleanup
 
 
-def title_from_position(matches):
+class TitleFromPosition(Rule):
     """
     Add title match in existing matches
-
-    :param matches:
-    :param context:
-    :return:
     """
-    filename = matches.markers.named('path', lambda marker: 'last' in marker.tags, 0)
-    start, end = filename.span
+    def when(self, matches, context):
+        filename = matches.markers.named('path', lambda marker: 'last' in marker.tags, 0)
+        start, end = filename.span
 
-    first_hole = matches.holes(start, end+1, formatter=cleanup, predicate=lambda hole: hole.value, index=0)
-    if first_hole:
-        first_hole.name = 'title'
-        matches.append(first_hole)
+        first_hole = matches.holes(start, end+1, formatter=cleanup, predicate=lambda hole: hole.value, index=0)
+        return first_hole
+
+    def then(self, matches, when_response, context):
+        when_response.name = 'title'
+        matches.append(when_response)
 
 
-TITLE = Rebulk().post_processor(title_from_position)
+TITLE = Rebulk().rules(TitleFromPosition)
