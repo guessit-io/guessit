9dff7d3ad7aed9ebe97a0a8033964e92fe0116dd
Toilal <toilal.dev@gmail.com>
2015-10-19 23:09:41 +0200

    Sort the longuer marker before when matches count is equal

--- guessit/rules/common/comparators.py
+++ guessit/rules/common/comparators.py
@@ -10,6 +10,8 @@ def marker_comparator(matches, markers):
     """
     Builds a comparator that returns markers sorted from the most valuable to the less.
 
+    Take the parts where matches count is higher, then when length is higher, then when position is at left.
+
     :param matches:
     :type matches:
     :return:
@@ -17,11 +19,14 @@ def marker_comparator(matches, markers):
     """
     def comparator(marker1, marker2):
         """
-        The actual comparator function
+        The actual comparator function.
         """
         matches_count = len(matches.range(*marker2.span)) - len(matches.range(*marker1.span))
         if matches_count:
             return matches_count
+        len_diff = len(marker2) - len(marker1)
+        if len_diff:
+            return len_diff
         return markers.index(marker2) - markers.index(marker1)
     return comparator
 
