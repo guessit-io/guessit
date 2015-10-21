9435bf9db5c9f6e3a290a0f3c99b0f1d7cf2cefc
Toilal <toilal.dev@gmail.com>
2015-10-21 23:11:46 +0200

    Let dot belong to extension

--- guessit/rules/extension.py
+++ guessit/rules/extension.py
@@ -8,11 +8,11 @@ from rebulk import Rebulk
 import regex as re
 
 EXTENSION = Rebulk().regex_defaults(flags=re.IGNORECASE)
-EXTENSION.defaults(name="extension")
+EXTENSION.defaults(name="extension", formatter=lambda value: value[1:])
 
-EXTENSION.regex(r'\.(\L<exts>)$', exts=['srt', 'idx', 'sub', 'ssa', 'ass'], tags=["subtitle"], children=True)
-EXTENSION.regex(r'\.(\L<exts>)$', exts=['nfo'], tags=["info"], children=True)
-EXTENSION.regex(r'\.(\L<exts>)$', exts=['3g2', '3gp', '3gp2', 'asf', 'avi', 'divx', 'flv', 'm4v', 'mk2',
+EXTENSION.regex(r'\.\L<exts>$', exts=['srt', 'idx', 'sub', 'ssa', 'ass'], tags=["subtitle"])
+EXTENSION.regex(r'\.\L<exts>$', exts=['nfo'], tags=["info"])
+EXTENSION.regex(r'\.\L<exts>$', exts=['3g2', '3gp', '3gp2', 'asf', 'avi', 'divx', 'flv', 'm4v', 'mk2',
                                         'mka', 'mkv', 'mov', 'mp4', 'mp4a', 'mpeg', 'mpg', 'ogg', 'ogm',
                                         'ogv', 'qt', 'ra', 'ram', 'rm', 'ts', 'wav', 'webm', 'wma', 'wmv',
-                                        'iso', 'vob'], tags=["video"], children=True)
+                                        'iso', 'vob'], tags=["video"])
