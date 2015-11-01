da989793178d1e9b6715dfa3e24ac441e198e923
Toilal <toilal.dev@gmail.com>
2015-11-01 20:15:37 +0100

    Add better conflict solver between screenSize and episodeNumber/season

--- guessit/rules/properties/screen_size.py
+++ guessit/rules/properties/screen_size.py
@@ -3,14 +3,17 @@
 """
 screenSize property
 """
-from rebulk import Rebulk
+from rebulk import Rebulk, Rule, RemoveMatch
 import regex as re
 
 from ..common.validators import seps_surround
 from guessit.rules.common import dash
 
 SCREEN_SIZE = Rebulk().regex_defaults(flags=re.IGNORECASE)
-SCREEN_SIZE.defaults(name="screenSize", validator=seps_surround)
+SCREEN_SIZE.defaults(name="screenSize", validator=seps_surround,
+                     conflict_solver=lambda match, other: '__default__'
+                     if other.name in ['screenSize', 'episodeNumber', 'season']
+                     else other)
 
 SCREEN_SIZE.regex(r"(?:\d{3,}(?:\\|\/|x|\*))?360(?:i|p?x?)", value="360p")
 SCREEN_SIZE.regex(r"(?:\d{3,}(?:\\|\/|x|\*))?368(?:i|p?x?)", value="368p")
@@ -30,5 +33,21 @@ SCREEN_SIZE.regex(r'\d{3,4}-?[x\*]-?\d{3,4}', abbreviations=[dash],
                   formatter=lambda value: 'x'.join(_digits_re.findall(value)),
                   conflict_solver=lambda match, other: match if other.name == 'screenSize' else other)
 
-# TODO: implement validators from guessit 1
-# validator=ChainedValidator(DefaultValidator(), OnlyOneValidator()))
+
+class ScreenSizeOnlyOne(Rule):
+    """
+    Keep a single screenSize pet filepath part.
+    """
+    consequence = RemoveMatch
+
+    def when(self, matches, context):
+        to_remove = []
+        for filepart in matches.markers.named('path'):
+            screensize = list(reversed(matches.range(filepart.start, filepart.end,
+                                                     lambda match: match.name == 'screenSize')))
+            if len(screensize) > 1:
+                to_remove.extend(screensize[1:])
+
+        return to_remove
+
+SCREEN_SIZE.rules(ScreenSizeOnlyOne)
