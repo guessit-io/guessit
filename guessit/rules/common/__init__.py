59a99a283d61fa9bce315665cca8e301d82e3302
Toilal <toilal.dev@gmail.com>
2015-11-28 15:03:22 +0100

    Remove ; from titles separators and & from separators

--- guessit/rules/common/__init__.py
+++ guessit/rules/common/__init__.py
@@ -5,9 +5,9 @@ Common module
 """
 from __future__ import unicode_literals
 
-seps = r' [](){}+*|&=§-_~#/\.,;:'  # list of tags/words separators
+seps = r' [](){}+*|=§-_~#/\.,;:'  # list of tags/words separators
 
-title_seps = r'-+/\|;'  # separators for title
+title_seps = r'-+/\|'  # separators for title
 
 dash = (r'-', r'[\W_]')  # abbreviation used by many rebulk objects.
 alt_dash = (r'@', r'[\W_]')  # abbreviation used by many rebulk objects.
