d0bd6c0b51cde554f9c610b0e0e80eb0cde98ea9
Toilal <toilal.dev@gmail.com>
2016-01-18 22:35:42 +0100

    Add more short words to exclusion list for language guessing
    Close #256

--- guessit/rules/common/words.py
+++ guessit/rules/common/words.py
@@ -30,7 +30,7 @@ COMMON_WORDS = frozenset([
     'fry', 'cop', 'zen', 'gay', 'fat', 'one', 'cherokee', 'got', 'an', 'as',
     'cat', 'her', 'be', 'hat', 'sun', 'may', 'my', 'mr', 'rum', 'pi', 'bb',
     'bt', 'tv', 'aw', 'by', 'md', 'mp', 'cd', 'lt', 'gt', 'in', 'ad', 'ice',
-    'ay', 'at', 'star', 'so', 'he',
+    'ay', 'at', 'star', 'so', 'he', 'do', 'ax', 'mx',
     # french words
     'bas', 'de', 'le', 'son', 'ne', 'ca', 'ce', 'et', 'que',
     'mal', 'est', 'vol', 'or', 'mon', 'se', 'je', 'tu', 'me',
