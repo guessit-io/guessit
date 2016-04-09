91f7441e6cb18d4f0e17ef434fc392a7ea7f0700
Toilal <toilal.dev@gmail.com>
2016-04-09 14:56:33 +0200

    Add support for titles containing dots
    Close #278

--- guessit/rules/common/formatters.py
+++ guessit/rules/common/formatters.py
@@ -20,25 +20,65 @@ for sep in seps:
 def cleanup(input_string):
     """
     Removes and strip separators from input_string (but keep ',;' characters)
+
+    It also keep separators for single characters (Mavels Agents of S.H.I.E.L.D.)
+
     :param input_string:
     :type input_string:
     :return:
     :rtype:
     """
+    clean_string = input_string
     for char in clean_chars:
-        input_string = input_string.replace(char, ' ')
-    return re.sub(' +', ' ', strip(input_string))
+        clean_string = clean_string.replace(char, ' ')
+
+    # Restore input separator if they separate single characters.
+    # Useful for Mavels Agents of S.H.I.E.L.D.
+    # https://github.com/guessit-io/guessit/issues/278
+
+    indices = [i for i, letter in enumerate(clean_string) if letter in seps]
+
+    dots = set()
+    if indices:
+        clean_list = list(clean_string)
+
+        potential_indices = []
+
+        for i in indices:
+            b = i-2
+            n = i+2
+            if b >= 0 and input_string[i] == input_string[b] and input_string[i-1] not in seps\
+                    and (n >= len(input_string) or input_string[n] == input_string[i] and input_string[n-1] not in seps):
+                potential_indices.append(i)
+
+        replace_indices = []
+
+        for potential_index in potential_indices:
+            if potential_index - 2 in potential_indices or potential_index + 2 in potential_indices:
+                replace_indices.append(potential_index)
+
+        if replace_indices:
+            for replace_index in replace_indices:
+                dots.add(input_string[replace_index])
+                clean_list[replace_index] = input_string[replace_index]
+            clean_string = ''.join(clean_list)
+
+    clean_string = strip(clean_string, ''.join([sep for sep in seps if sep not in dots]))
+
+    clean_string = re.sub(' +', ' ', clean_string)
+    return clean_string
 
 
-def strip(input_string):
+def strip(input_string, chars=seps):
     """
     Strip separators from input_string
     :param input_string:
+    :param chars:
     :type input_string:
     :return:
     :rtype:
     """
-    return input_string.strip(seps)
+    return input_string.strip(chars)
 
 
 def raw_cleanup(raw):
