efd37f7e7f3519ff00b34f8d37630978013968ad
Toilal <toilal.dev@gmail.com>
2016-11-26 15:44:23 +0100

    Guess full name streaming services

--- guessit/rules/properties/streaming_service.py
+++ guessit/rules/properties/streaming_service.py
@@ -3,10 +3,12 @@
 """
 streaming_service property
 """
+import re
+
 from rebulk import Rebulk
 from rebulk.rules import Rule, RemoveMatch
 
-from ...rules.common import seps
+from ...rules.common import seps, dash
 from ...rules.common.validators import seps_surround
 
 
@@ -16,36 +18,45 @@ def streaming_service():
     :return:
     :rtype: Rebulk
     """
-    rebulk = Rebulk().string_defaults(ignore_case=True)
+    rebulk = Rebulk().string_defaults(ignore_case=True).regex_defaults(flags=re.IGNORECASE, abbreviations=[dash])
     rebulk.defaults(name='streaming_service', validator=seps_surround)
 
-    rebulk.string('AE', value='A&E')
+    rebulk.string('AE', 'A&E', value='A&E')
     rebulk.string('AMBC', value='ABC')
-    rebulk.string('AMZN', value='Amazon Prime')
-    rebulk.string('AS', value='Adult Swim')
-    rebulk.string('iP', value='BBC iPlayer')
+    rebulk.string('AMZN', 'AmazonPrime', value='Amazon Prime')
+    rebulk.regex('Amazon-Prime', value='Amazon Prime')
+    rebulk.string('AS', 'AdultSwim', value='Adult Swim')
+    rebulk.regex('Adult-Swim', value='Adult Swim')
+    rebulk.string('iP', 'BBCiPlayer', value='BBC iPlayer')
+    rebulk.regex('BBC-iPlayer', value='BBC iPlayer')
     rebulk.string('CBS', value='CBS')
-    rebulk.string('CC', value='Comedy Central')
-    rebulk.string('CR', value='Crunchy Roll')
-    rebulk.string('CW', value='The CW')
-    rebulk.string('DISC', value='Discovery')
-    rebulk.string('DSNY', value='Disney')
-    rebulk.string('EPIX', value='ePix')
-    rebulk.string('HBO', value='HBO Go')
-    rebulk.string('HIST', value='History')
-    rebulk.string('IFC', value='IFC')
-    rebulk.string('PBS', value='PBS')
-    rebulk.string('NATG', value='National Geographic')
-    rebulk.string('NBA', value='NBA TV')
+    rebulk.string('CC', 'ComedyCentral', value='Comedy Central')
+    rebulk.regex('Comedy-Central', value='Comedy Central')
+    rebulk.string('CR', 'CrunchyRoll', value='Crunchy Roll')
+    rebulk.regex('Crunchy-Roll', value='Crunchy Roll')
+    rebulk.string('CW', 'TheCW', value='The CW')
+    rebulk.regex('The-CW', value='The CW')
+    rebulk.string('DISC', 'Discovery', value='Discovery')
+    rebulk.string('DSNY', 'Disney', value='Disney')
+    rebulk.string('EPIX', 'ePix', value='ePix')
+    rebulk.string('HBO', 'HBOGo', value='HBO Go')
+    rebulk.regex('HBO-Go', value='HBO Go')
+    rebulk.string('HIST', 'History', value='History')
+    rebulk.string('IFC', 'IFC', value='IFC')
+    rebulk.string('PBS', 'PBS', value='PBS')
+    rebulk.string('NATG', 'NationalGeographic', value='National Geographic')
+    rebulk.regex('National-Geographic', value='National Geographic')
+    rebulk.string('NBA', 'NBATV', value='NBA TV')
+    rebulk.regex('NBA-TV', value='NBA TV')
     rebulk.string('NBC', value='NBC')
     rebulk.string('NFL', value='NFL')
-    rebulk.string('NICK', value='Nickelodeon')
-    rebulk.string('NF', value='Netflix')
-    rebulk.string('SESO', value='SeeSo')
-    rebulk.string('SPKE', value='Spike TV')
-    rebulk.string('SYFY', value='Syfy')
-    rebulk.string('TFOU', value='TFou')
-    rebulk.string('TVL', value='TV Land')
+    rebulk.string('NICK', 'Nickelodeon', value='Nickelodeon')
+    rebulk.string('NF', 'Netflix', value='Netflix')
+    rebulk.string('SESO', 'SeeSo', value='SeeSo')
+    rebulk.string('SPKE', 'SpikeTV', 'Spike TV', value='Spike TV')
+    rebulk.string('SYFY', 'Syfy', value='Syfy')
+    rebulk.string('TFOU', 'TFou', value='TFou')
+    rebulk.string('TVL', 'TVLand', 'TV Land', value='TV Land')
     rebulk.string('UFC', value='UFC')
 
     rebulk.rules(ValidateStreamingService)
