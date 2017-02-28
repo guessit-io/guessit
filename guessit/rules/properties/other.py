660c99f12f77a186add402bdfca8cacfe24e8ac5
Rato <rato.aq2@gmail.com>
2017-02-28 20:54:51 +0100

    Detecting FESTIVAL

--- guessit/rules/properties/other.py
+++ guessit/rules/properties/other.py
@@ -80,6 +80,7 @@ def other():
     rebulk.regex('Alternat(e|ive)(?:-?Cut)?', value='Alternative Cut', tags=['has-neighbor', 'release-group-prefix'])
     rebulk.string('CONVERT', value='Converted', tags='has-neighbor')
     rebulk.string('DOCU', value='Documentary', tags='has-neighbor')
+    rebulk.string('Festival', value='Festival', tags=['has-neighbor-before', 'has-neighbor-after'])
     rebulk.string('Complet', value='Complete', tags=['has-neighbor', 'release-group-prefix'])
 
     rebulk.string('VO', 'OV', value='OV', tags='has-neighbor')
